b6fd8085a65dfce5a4a16c3f99e6514bc6c70176
Rania Benaissa <ranbenaissa@gmail.com>
2022-01-30 23:54:15 +0100

    hex_grid update

--- Hexa_grid.py
+++ Hexa_grid.py
@@ -1,6 +1,8 @@
+
 from skimage import io
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 from skimage import color
 import cv2
 # centre = centre de l'hexa
@@ -8,79 +10,105 @@ import cv2
 # i = corner nbr (6 in total)
 
 
-def hex_corner(center, size, i):
+def hexPoint(center, size, i):
+
     degree_angle = 60 * i
     rad_angle = np.deg2rad(degree_angle)
     return (int(center[0] + size * np.cos(rad_angle)),
             int(center[1] + size * np.sin(rad_angle)))
 
 
-def drawHexa(image, center, size):
+def drawHexa(image, center, size, color=(255, 255, 255)):
 
-    color = (55, 46, 101)
+    # color = (55, 46, 101)
 
-    thickness = 2
+    thickness = 1
 
-    points = []
-
-    points.append(hex_corner(center, size, 0))
+    points = getHexaPoints(center, size)
 
     for i in range(1, 6):
 
-        points.append(hex_corner(center, size, i))
-
         image = cv2.line(
-            image, points[-2], points[-1], color, thickness, cv2.LINE_AA)
+            image, points[i-1], points[i], color, thickness, cv2.LINE_AA)
 
-    # dernier point
+    # last point
     image = cv2.line(image, points[0], points[-1],
                      color, thickness, cv2.LINE_AA)
 
-    print("Hexa", points, "\n")
     return image
 
 
-def drawGrid(image, sigma):
+def getHexaPoints(center, size):
 
-    h, w, _ = image.shape
+    points = []
+
+    for i in range(0, 6):
+
+        points.append(hexPoint(center, size, i))
+
+    return points
+
+
+def isHexInImage(w, h, center, size):
+
+    # car parfois y a de mini pixels qui flood je sais pas si je dois les considerer
+    epsilon = 5
+
+    points = getHexaPoints(center, size)
+
+    for point in points:
+
+        x, y = point
+
+        if(x >= 0 and x+epsilon < w and y >= 0 and y+epsilon < h):
+
+            return True
+    return False
+
+
+def drawGrid(image, sigma, color=(255, 255, 255)):
+    # color image
+    if(len(image.shape) == 3):
+
+        h, w, _ = image.shape
+    # grayscale image
+    else:
+        h, w = image.shape
 
     change = 1
 
     centers = []
 
-    w_padd = (w % sigma)/2
+    size = sigma / np.sqrt(3)
 
-    h_padd = (h % sigma)/2
+    center = (0, 0)
 
-    init_x = w_padd + sigma
+    while isHexInImage(w, h, center, size):
 
-    init_y = h_padd + sigma
+        while(isHexInImage(w, h, center, size)):
 
-    center = (init_x, init_y)
-
-    centers.append(center)
-
-    for i in range(1, int((w / sigma))):
-
-        for j in range(1, int((h / sigma))):
-
-            print("center", center, "\n")
-
-            image = drawHexa(image, center, sigma)
-
-            # The sqrt(3) comes from sin(60°)
-            center = (center[0], center[1] + np.sqrt(3) * sigma)
+            image = drawHexa(image, center, size)
 
             centers.append(center)
 
-        center = (init_x + i * 3/2 * sigma,
-                  init_y - change * (np.sqrt(3) * sigma)/2)
+            cv2.circle(image, (int(center[0]), int(
+                center[1])), 0, color, 5)
+
+            # The sqrt(3) comes from sin(60°)
+            center = (center[0], center[1] + sigma)
+
+        center = (center[0] + (3/2)*size,
+                  change*sigma/2)
 
         if(change == 0):
+
             change = 1
+
         else:
 
             change = 0
+
+    print(len(centers))
 
     plt.imshow(image, cmap='gray')
 
@@ -88,11 +116,11 @@ def drawGrid(image, sigma):
 
 
 # img = io.imread('image.jpg')
-img = io.imread('image.jpg')
+img = io.imread('lena.jpg')
 
 
-# conversion to gray scale image
+# conversion to grxay scale image
 gray_img = color.rgb2gray(img)
 
 
-drawGrid(img, 15)
+drawGrid(img, 45)
